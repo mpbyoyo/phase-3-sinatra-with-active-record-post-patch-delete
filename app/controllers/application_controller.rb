42376af136f9f4eedfb30bb0934c4c65097eadce
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-06-17 13:35:09 -0500

    commit tree 5dafd5d68f1d48902a84a1b2c1631ae56017a0c9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,36 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  get '/reviews' do
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  get '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    Review.find(params[:id]).update(score: params[:score], comment: params[:comment])
+    Review.find(params[:id]).to_json
+  end
+  
 end
